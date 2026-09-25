202b7a95509d99b1b1208f29ee6b37ff9fecb03e
Felicia224-K <lainemamysoumaoro@gmail.com>
2026-09-25 13:35:10 +0000

    fix: the server port by removing the 0s

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,6 +6,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`TactusBook API running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`TactusBook API running on port ${PORT}`);
 });
