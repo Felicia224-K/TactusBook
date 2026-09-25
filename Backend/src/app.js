8840a910f686eabb1dcbc7e5d8544d66f9261c4e
Felicia224-K <lainemamysoumaoro@gmail.com>
2026-09-25 16:32:42 +0000

    fix: update backend to allow use of the app in other devices

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,20 @@ const app = express();
 
 const db = require ('../src/config/database')
 
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors( {
+  origin: [
+
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [ 'Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 app.use('/api/auth', require('../src/routes/auth'));
